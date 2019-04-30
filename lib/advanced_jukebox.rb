bbc2701eda4293a9f1eac0d8d366214c36257735
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-30 03:42:31 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,9 +13,12 @@
 # }
 
 def help
-  #this method should be the same as in jukebox.rb
-
-end
+  puts "I accept the following commands:
+    - help : displays this help message
+    - list : displays a list of songs you can play
+    - play : lets you choose a song to play
+    - exit : exits this program"
+end  
 
 
 
